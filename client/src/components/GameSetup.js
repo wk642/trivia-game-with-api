9317155f8fc201a02f61a758fb96e8e6ffd1ca04
Winnie Kelley <w642kelley@gmail.com>
2025-03-05 14:57:02 -0600

    setting up GameSetup.js

---
+++ client/src/components/GameSetup.js
@@ -0,0 +1,16 @@
+import React, { useRef } from "react";
+import "../App.css";
+
+const GameSetup = () => {
+  // useRef
+  const numOfQuestions = useRef();
+  const questionType = useRef();
+  const difficulty = useRef();
+  const category = useRef();
+
+  // handle submit
+
+  // storing inputs
+
+  // return form
+}
