e14b998da3ab6a34d94a359ab2a2e4eed89ff8bb
Winnie Kelley <w642kelley@gmail.com>
2025-03-06 16:39:43 -0600

    Added categories dropdown

--- client/src/components/GameSetup.jsx
+++ client/src/components/GameSetup.jsx
@@ -5,6 +5,7 @@ function GameSetup({ onStartGame }) {
   const [numberOfQuestions, setNumberOfQuestions] = useState(10);
   const questionType = useRef('multiple-choice');
   const questionDifficulty = useRef('medium');
+  const questionCategory = useRef('any-category');
 
   const handleStartGame = () => {
     const gameSettings = {
@@ -72,10 +73,36 @@ function GameSetup({ onStartGame }) {
         </button>
       </div>
 
-      <div>
+      <div className="category-div">
         <label>Question Category:</label>
         <br/>
-        
+        <select>
+          <option>Any Category</option>
+          <option>General Knowledge</option>
+          <option>Entertainment: Books</option>    
+          <option>Entertainment: Film</option>
+          <option>Entertainment: Music</option>
+          <option>Entertainment: Musical & Theaters</option>
+          <option>Entertainment: Television</option>
+          <option>Entertainment: Video Games</option>
+          <option>Entertainment: Board Games</option>
+          <option>Science & Nature</option>
+          <option>Science: Computers</option>
+          <option>Science: Matheetics</option>
+          <option>Mythology</option>
+          <option>Sports</option>
+          <option>Geography</option>
+          <option>History</option>
+          <option>Politics</option>
+          <option>Art</option>
+          <option>Celebreties</option>
+          <option>Animals</option>
+          <option>Vehicles</option>
+          <option>Entertainment: Cosmics</option>
+          <option>Science: Gadgets</option>
+          <option>Entertainment: Japanese Anime & Manga</option>
+          <option>Entertainment: Cartoon & Animations</option>
+        </select>        
       </div>
 
       <button onClick={handleStartGame}>Start Game</button>
