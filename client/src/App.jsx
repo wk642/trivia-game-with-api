ef7f61e19be8d7521f8bed05b9e919c485ca150b
Winnie Kelley <w642kelley@gmail.com>
2025-03-09 16:49:08 -0500

    Adding this incase i run into the 429 error again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 import './App.css'
 import GameSetup from './components/GameSetup'
 import GamePlay from './components/GamePlay'
+import GameResult from './components/GameResult'
 
 function App() {
   return (
-
       <div>
         <GameSetup />
         <GamePlay />
+        <GameResult />
       </div>
-
   )
 }
 
---
+++ client/src/components/GameResult.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function GameResult() {
+  return (
+    <div>
+      <div className="score-div">
+        score
+      </div>
+
+      <div className="message-div">
+        win / loss message
+      </div>
+    </div>
+  )
+}
+
+export default GameResult;
