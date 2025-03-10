2b8fa96c4e1033ac28fb40f9f6684a6b01bab115
Winnie Kelley <w642kelley@gmail.com>
2025-03-10 12:57:47 -0500

    udpated some css and formatting

--- client/src/components/GameResult.jsx
+++ client/src/components/GameResult.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import './GameResult.css';
 
 function GameResult({ score, totalQuestions, onRestart }) {
-    const winThreshold = totalQuestions / 2;
-    const gameWon = score > winThreshold;
+  const winThreshold = totalQuestions / 2;
+  const gameWon = score > winThreshold;
 
-    return (
-        <div className="game-result-container">
-            <h2>Game Over!</h2>
-            {gameWon ? (
-                <p>Congratulations! You Win!</p>
-            ) : (
-                <p>Better luck next time! You Lose.</p>
-            )}
-            <p>Your score: {score} / {totalQuestions}</p>
-            <button onClick={onRestart}>Restart Game</button>
-        </div>
-    );
+  return (
+    <div className="game-result-container">
+      {/* Displaying the correct message */}
+      <div className="win-lose-message">
+      <h2>Game Over!</h2>
+        {gameWon ? (
+          <h2>Congratulations! You Win!</h2>
+        ) : (
+          <h2>Better luck next time! You Lose.</h2>
+        )}
+
+        <h2>Your score: {score} / {totalQuestions}</h2>
+      </div>
+
+      <div className="restart-game-div">
+        <button onClick={onRestart}>Restart Game</button>
+      </div>
+    </div>
+  );
 }
 
 export default GameResult;
--- client/src/components/GameSetup.jsx
+++ client/src/components/GameSetup.jsx
@@ -2,125 +2,125 @@ import React, { useRef, useState } from 'react';
 import './GameSetup.css';
 
 function GameSetup({ startGame }) {
-    const [numberOfQuestions, setNumberOfQuestions] = useState('10');
-    const questionType = useRef('multiple');
-    const questionDifficulty = useRef('medium');
-    const questionCategory = useRef('');
+  const [numberOfQuestions, setNumberOfQuestions] = useState('10');
+  const questionType = useRef('multiple');
+  const questionDifficulty = useRef('medium');
+  const questionCategory = useRef('');
 
-    const changeNumberOfQuestions = (event) => {
-        setNumberOfQuestions(parseInt(event.target.value));
+  const changeNumberOfQuestions = (event) => {
+    setNumberOfQuestions(parseInt(event.target.value));
+  };
+
+  const changeQuestionType = (type) => {
+    questionType.current = type;
+  };
+
+  const changeDifficulty = (difficulty) => {
+    questionDifficulty.current = difficulty;
+  };
+
+  const handleStartGame = () => {
+    const gameSettings = {
+      amount: numberOfQuestions,
+      type: questionType.current,
+      difficulty: questionDifficulty.current,
+      category: questionCategory.current.value,
     };
 
-    const changeQuestionType = (type) => {
-        questionType.current = type;
-    };
+    console.log("Game Settings:", gameSettings);
 
-    const changeDifficulty = (difficulty) => {
-        questionDifficulty.current = difficulty;
-    };
+    if (startGame) {
+      startGame(gameSettings);
+    }
+  };
 
-    const handleStartGame = () => {
-        const gameSettings = {
-            amount: numberOfQuestions,
-            type: questionType.current,
-            difficulty: questionDifficulty.current,
-            category: questionCategory.current.value,
-        };
-
-        console.log("Game Settings:", gameSettings);
-
-        if (startGame) {
-            startGame(gameSettings);
-        }
-    };
-
-    return (
-      <div className="game-setup-container">
-        <h2>Game Setup</h2>
-        {/* number of questions slider */}
-        <div className="number-of-questions-div">
-          <label>Number of Questions: {numberOfQuestions}</label>
-          <br />
-          <input
-            type="range"
-            id="numQuestions"
-            min="1"
-            max="15"
-            value={numberOfQuestions}
-            onChange={changeNumberOfQuestions}
-          />
-        </div>
-
-        {/* question type, multiple choice / true false */}
-        <div className="question-type-div">
-          <button 
-            onClick={() => changeQuestionType('multiple')}
-            className={questionType === 'boolean' ? 'selected' : ''}
-          >
-            Multiple Choice
-          </button>
-          <button 
-            onClick={() => changeQuestionType('boolean')}
-            className={questionType === 'boolean' ? 'selected' : ''}
-          >
-            True/False
-          </button>
-        </div>
-
-        {/* difficulty levels: easy, medium, hard */}
-        <div className="difficulty-div">
-            <button 
-              onClick={() => changeDifficulty('easy')}
-              className={questionDifficulty === 'easy' ? 'selected' : ''}
-            >
-              Easy
-            </button>
-            <button 
-              onClick={() => changeDifficulty('medium')}
-              className={questionDifficulty === 'medium' ? 'selected' : ''}
-            >
-              Medium
-            </button>
-            <button 
-              onClick={() => changeDifficulty('hard')}
-              className={questionDifficulty === 'easy' ? 'selected' : ''}
-            >
-              Hard
-            </button>
-        </div>
-
-        <div className="category-div">
-            <select ref={questionCategory}>
-                <option value=''>Any Category</option>
-                <option value='9'>General Knowledge</option>
-                <option value='10'>Entertainment: Books</option>
-                <option value='11'>Entertainment: Film</option>
-                <option value='12'>Entertainment: Music</option>
-                <option value='13'>Entertainment: Musical & Theaters</option>
-                <option value='14'>Entertainment: Television</option>
-                <option value='15'>Entertainment: Video Games</option>
-                <option value='16'>Entertainment: Board Games</option>
-                <option value='17'>Science & Nature</option>
-                <option value='18'>Science: Computers</option>
-                <option value='19'>Science: Mathematics</option>
-                <option value='20'>Mythology</option>
-                <option value='21'>Sports</option>
-                <option value='22'>Geography</option>
-                <option value='23'>History</option>
-                <option value='24'>Politics</option>
-                <option value='25'>Art</option>
-                <option value='26'>Celebreties</option>
-                <option value='27'>Animals</option>
-                <option value='28'>Vehicles</option>
-                <option value='29'>Entertainment: Cosmics</option>
-                <option value='30'>Science: Gadgets</option>
-                <option value='31'>Entertainment: Japanese Anime & Manga</option>
-                <option value='32'>Entertainment: Cartoon & Animations</option>
-            </select>
-        </div>
-
-        <button onClick={handleStartGame}>Start Game</button>
+  return (
+    <div className="game-setup-container">
+      <h2>Game Setup</h2>
+      {/* number of questions slider */}
+      <div className="number-of-questions-div">
+        <label>Number of Questions: {numberOfQuestions}</label>
+        <br />
+        <input
+          type="range"
+          id="numQuestions"
+          min="1"
+          max="15"
+          value={numberOfQuestions}
+          onChange={changeNumberOfQuestions}
+        />
       </div>
+
+      {/* question type, multiple choice / true false */}
+      <div className="question-type-div">
+        <button 
+          onClick={() => changeQuestionType('multiple')}
+          className={questionType === 'boolean' ? 'selected' : ''}
+        >
+          Multiple Choice
+        </button>
+        <button 
+          onClick={() => changeQuestionType('boolean')}
+          className={questionType === 'boolean' ? 'selected' : ''}
+        >
+          True/False
+        </button>
+      </div>
+
+      {/* difficulty levels: easy, medium, hard */}
+      <div className="difficulty-div">
+        <button 
+          onClick={() => changeDifficulty('easy')}
+          className={questionDifficulty === 'easy' ? 'selected' : ''}
+        >
+          Easy
+        </button>
+        <button 
+          onClick={() => changeDifficulty('medium')}
+          className={questionDifficulty === 'medium' ? 'selected' : ''}
+        >
+          Medium
+        </button>
+        <button 
+          onClick={() => changeDifficulty('hard')}
+          className={questionDifficulty === 'easy' ? 'selected' : ''}
+        >
+          Hard
+        </button>
+      </div>
+
+      <div className="category-div">
+        <select ref={questionCategory}>
+          <option value=''>Any Category</option>
+          <option value='9'>General Knowledge</option>
+          <option value='10'>Entertainment: Books</option>
+          <option value='11'>Entertainment: Film</option>
+          <option value='12'>Entertainment: Music</option>
+          <option value='13'>Entertainment: Musical & Theaters</option>
+          <option value='14'>Entertainment: Television</option>
+          <option value='15'>Entertainment: Video Games</option>
+          <option value='16'>Entertainment: Board Games</option>
+          <option value='17'>Science & Nature</option>
+          <option value='18'>Science: Computers</option>
+          <option value='19'>Science: Mathematics</option>
+          <option value='20'>Mythology</option>
+          <option value='21'>Sports</option>
+          <option value='22'>Geography</option>
+          <option value='23'>History</option>
+          <option value='24'>Politics</option>
+          <option value='25'>Art</option>
+          <option value='26'>Celebreties</option>
+          <option value='27'>Animals</option>
+          <option value='28'>Vehicles</option>
+          <option value='29'>Entertainment: Cosmics</option>
+          <option value='30'>Science: Gadgets</option>
+          <option value='31'>Entertainment: Japanese Anime & Manga</option>
+          <option value='32'>Entertainment: Cartoon & Animations</option>
+        </select>
+      </div>
+
+      <button onClick={handleStartGame}>Start Game</button>
+    </div>
   );
 }
 
