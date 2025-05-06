5dc2b4a35e61a89b32776f5e1109c2b87467899f
Winnie Kelley <w642kelley@gmail.com>
2025-05-06 16:55:43 -0500

    Fixed testing dependencies

--- client/src/components/GameSetup.test.jsx
+++ client/src/components/GameSetup.test.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import GameSetup from './GameSetup'; 
+import '@testing-library/jest-dom';
+
 
 describe('GameSetup Component', () => {
-  test('to see if it renders the game', () => {
+  it('to see if it renders the game', () => {
     render(<GameSetup />);
   });
 });
